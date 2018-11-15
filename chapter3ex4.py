8ddad408d239ac6511e264ef552145014b1642e7
Mateusz <39599753+matiasek@users.noreply.github.com>
2018-11-15 16:46:02 +0100

    Add files via upload

---
+++ chapter3ex4.py
@@ -0,0 +1,15 @@
+﻿def IsPrime(num):
+    counter=0
+    for i in range(1,num+1):
+        if num%i==0:
+            counter+=1
+    if counter > 2:
+        return False
+    else:
+        return True
+            
+print("Liczby pierwsze to")
+for i in range(1,200):
+	if IsPrime(i+1)==True:
+			print(i+1,end=" ")
+print()
