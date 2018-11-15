5371bfa02cb99903f575876023b11f6073d19ab3
Mateusz <39599753+matiasek@users.noreply.github.com>
2018-11-15 16:46:55 +0100

    Update chapter3ex4.py

--- chapter3ex4.py
+++ chapter3ex4.py
@@ -1,4 +1,21 @@
-﻿def IsPrime(num):
+'''Your task is to write a function checking whether a number is prime or not.
+
+The function:
+
+is called IsPrime;
+takes one argument (the value to check)
+returns True if the argument is a prime number, and False otherwise.
+Hint: try to divide the argument by all subsequent values (starting from 2) and check the remainder - if it's zero, your number cannot be a prime; think carefully about when you should stop the process.
+
+If you need to know the square root of any value you can utilize the ** operator. Remember: the square root of x is the same as x0.5
+
+Complete the code presented below.
+
+Run your code and check whether your output is the same as ours.
+
+2 3 5 7 11 13 17 19 '''
+
+def IsPrime(num):
     counter=0
     for i in range(1,num+1):
         if num%i==0:
